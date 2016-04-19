df0f7cc33b54274a58183cc02a711d7378ef9f5c
Chris Boertien <chris.boertien@gmail.com>
2016-04-19 15:24:14 -0300

    listen on 3000
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,6 @@
 @dir = File.expand_path(File.dirname(__FILE__))
 listen File.join(@dir, "../unicorn.sock"), :backlog => 1024
+listen 3000
 
 stderr_path "/usr/src/app/log/unicorn.stderr.log"
 stdout_path "/usr/src/app/log/unicorn.stdout.log"
